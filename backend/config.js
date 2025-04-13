4422b1a20c81f39934e4df4af48548e411669d88
2024-DS-31 <inspirationalhubb75@gmail.com>
2025-04-13 22:25:55 +0500

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root75@book-store-mern.iciw3d2.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
